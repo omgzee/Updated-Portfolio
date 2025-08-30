a5a9d7f0ec40abc53c49cb64ea32dbe0550fd193
omgzee <hallazee@outlook.com>
2025-08-30 19:08:55 +0530

    Updated site content

--- src/MetaTags.tsx
+++ src/MetaTags.tsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import { AboutMeData } from "./types";
+import { AboutMeData } from "./types"; // adjust import path if needed
 
-const MetaTags: React.FC<{ info: AboutMeData }> = ({ info }) => {
-    return (
-        <Helmet>
-            <title>{info.name}'s portfolio</title>
-            <meta name="description" content={info.introduction} />
-            <meta property="og:image" content={`${process.env.PUBLIC_URL}/favicon.ico`} />
-        </Helmet>
-    );
+// Define props
+type Props = {
+  info: AboutMeData;
+};
+
+const MetaTags: React.FC<Props> = ({ info }) => {
+  return (
+    <Helmet>
+      <title>{info.name}'s Portfolio</title>
+      <meta name="description" content={info.introduction} />
+
+      {/* Open Graph tags */}
+      <meta property="og:title" content={`${info.name}'s Portfolio`} />
+      <meta property="og:description" content={info.introduction} />
+      <meta
+        property="og:image"
+        content={`${process.env.PUBLIC_URL}/preview.jpg`}
+      />
+      <meta property="og:url" content="https://zeeshan.world" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter card */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={`${info.name}'s Portfolio`} />
+      <meta name="twitter:description" content={info.introduction} />
+      <meta
+        name="twitter:image"
+        content={`${process.env.PUBLIC_URL}/preview.jpg`}
+      />
+    </Helmet>
+  );
 };
 
 export default MetaTags;
